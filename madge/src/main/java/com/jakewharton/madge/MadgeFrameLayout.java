59d822942918cc38cbbfa2874a11509920b9c172
Jake Wharton <jw@squareup.com>
2014-01-03 01:01:32 -0800

    Suppress API warning with target API annotation.

--- madge/src/main/java/com/jakewharton/madge/MadgeFrameLayout.java
+++ madge/src/main/java/com/jakewharton/madge/MadgeFrameLayout.java
@@ -78,6 +78,7 @@ public final class MadgeFrameLayout extends FrameLayout {
     }
   }
 
+  @TargetApi(HONEYCOMB)
   @Override public boolean isHardwareAccelerated() {
     return !enabled && super.isHardwareAccelerated();
   }
